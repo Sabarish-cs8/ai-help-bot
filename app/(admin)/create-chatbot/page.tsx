1041dceca4c49980774cbe1a0558cfcc01317a44
sabarish <sabarish.cs8@gmail.com>
2024-12-07 18:26:28 +0530

    Add sidebar

---
+++ app/(admin)/create-chatbot/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CreateChatbot() {
+  return (
+    <div>CreateChatbot</div>
+  )
+}
+
+export default CreateChatbot
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,30 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 import Avatar from './Avatar'
 import Link from 'next/link'
 
 function Header() {
   return (
-    <header>
-        <Link href="/">
+    <header className='bg-white shadow-sm text-gray-800 flex justify-between p-5'>
+        <Link href="/" className='flex items-center text-4xl font-thin'>
         {/**Avartar */}
         <Avatar 
         seed='Heartbreak support Agent'
         />
-        <div>
+        <div className='space-y-1'>
             <h1>Classic</h1>
             <h2 className='text-sm'>Your Customisable AI Chat Agent</h2>
         </div>
         </Link>
+
+        <div className='flex items-center'>
+            <SignedIn>
+                <UserButton showName />
+            </SignedIn>
+
+            <SignedOut>
+                <SignInButton />
+            </SignedOut>
+        </div>
     </header>
   )
 }
