3612ff5847f4848d29db8bce1f93e66ed62f95f5
sabarish <sabarish.cs8@gmail.com>
2024-12-10 20:02:39 +0530

    Add -> ChatBotSessions

--- app/(admin)/review-sessions/page.tsx
+++ app/(admin)/review-sessions/page.tsx
@@ -1,4 +1,7 @@
+import ChatBotSessions from "@/components/ChatBotSessions";
+import { GET_USER_CHATBOTS } from "@/graphql/queries/queries";
 import { serverClient } from "@/lib/server/serverClient";
+import { Chatbot, GetUserChatbotsResponse, GetUserChatbotsVariables } from "@/types/types";
 import { auth } from "@clerk/nextjs/server";
 
 async function ReviewSessions() {
@@ -12,10 +15,23 @@ async function ReviewSessions() {
     GetUserChatbotsVariables
     >({
         query:GET_USER_CHATBOTS,
-        variables:{userId:userId},
+        variables:{ userId },
     });
+
+    const sortedChatbotsByUser:Chatbot[] = chatbotsByUser.map((chatbot)=>({ ...chatbot,chat_sessions:[...chatbot.chat_sessions].sort((a,b) =>
+    //sort in ascending order
+    new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+     ),
+    }));
   return (
-    <div>ReviewSessions</div>
+    <div className="flex-1 px-10">
+        <h1 className="text-xl lg:text-3xl font-semibold mt-10">Chat Sessions
+        </h1>
+        <h2 className="mb-5">
+            Review all the chat sessions the chat bots have had with your customers.
+        </h2>
+        <ChatBotSessions chatbots={sortedChatbotsByUser} />
+    </div>
   )
 }
 
