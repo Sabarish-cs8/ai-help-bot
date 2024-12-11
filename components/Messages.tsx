a06144fbfceb90f4b2e8e0af55b364c746e468f8
sabarish <sabarish.cs8@gmail.com>
2024-12-11 12:06:35 +0530

    Add Messages page

--- components/Messages.tsx
+++ components/Messages.tsx
@@ -2,21 +2,31 @@
 
 import { Message } from '@/types/types';
 import { usePathname } from 'next/navigation';
-import React from 'react'
 import Avatar from './Avatar';
 import { UserCircle } from 'lucide-react';
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import { useEffect, useRef } from 'react';
 
-function Messages({messages , chatbotName}:{
+function Messages({
+  messages, 
+  chatbotName
+}:{
     messages:Message[];
     chatbotName:string;
 }) {
-   
-
+  const ref = useRef<HTMLDivElement>(null);
   const path =usePathname();
   const isReviewPage=path.includes("review-sessions");
 
+  useEffect(()=>{
+    if (ref.current){
+      ref.current.scrollIntoView({behavior:"smooth"});
+    }
+  },[messages])
+
     return (
-    <div>
+    <div className="flex-1 flex flex-col overflow-y-auto space-y-10 py-10 px-5 bg-white rounded-lg">
       {messages.map((message)=>{
         const isSender = message.sender !== "user";
 
@@ -28,7 +38,7 @@ function Messages({messages , chatbotName}:{
                 </p>
               )}
 
-              <div>
+              <div className={`chat-image avatar w-10 ${!isSender && "-mr-4"}`}>
                 {isSender ? (
                   <Avatar seed={chatbotName}
                   className='h-12 w-12 bg-white rounded-full border-2 border-[#2991EE]' />
@@ -39,11 +49,69 @@ function Messages({messages , chatbotName}:{
 
               <p className={`chat-bubble text-white ${isSender 
               ? "chat-bubble-primary bg-[#4D7DFB]"
-              : "chat-bubble-secondary bg-gray-200 text-gray-700" }`}>Hello!!!
+              : "chat-bubble-secondary bg-gray-200 text-gray-700" }`}>
+                <ReactMarkdown 
+                  remarkPlugins={[remarkGfm]}
+                  className={`break-words`}
+                  components={{
+                    ul:({node,...props}) =>(
+                      <ul {...props}
+                      className='list-disc list-inside ml-5 mb-5'
+                      />
+                    ),
+                    ol:({node,...props})=>(
+                      <ol {...props}
+                      className='list-decimal list-inside ml-5 mb-5'
+                      />
+                    ),
+                    h1:({node,...props})=>(
+                      <h1 {...props}
+                      className='text-2xl font-bold mb-5'
+                      />
+                    ),
+                    h2:({node,...props})=>(
+                      <h2 {...props}
+                      className='text-xl font-bold mb-5'
+                      />
+                    ),
+                    h3:({node,...props})=>(
+                      <h3 {...props}
+                      className='text-lg font-bold mb-5'
+                      />
+                    ),
+                    table:({node,...props})=>(
+                      <table {...props}
+                      className='table-auto w-full border-separate border-2 rounded-sm border-spacing-4 border-white mb-5'
+                      />
+                    ),
+                    th:({node,...props})=>(
+                      <th {...props}
+                      className='text-left underline'
+                      />
+                    ),
+                    p:({node,...props})=>(
+                      <p {...props}
+                      className={`whitespace-break-space mb-5 ${
+                        message.content === "Thinking..." && "animate-pulse"} ${isSender ? "text-white" : "text-gray-700"}`}
+                      />
+                    ),
+                    a:({node,...props})=>(
+                      <a {...props}
+                      target='_blank'
+                      className='font-bold underline hover:text-blue-400'
+                      rel='noopener noreferrer'
+                      />
+                    ),
+                  }}
+                >
+                {message.content}
+                </ReactMarkdown>
               </p>
           </div>
         )
       })}
+
+      <div ref={ref} />
       
     </div>
   )
