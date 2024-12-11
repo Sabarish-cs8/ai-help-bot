f80348fa7fe3001a4d1eb108b6e28957dfda16ed
sabarish <sabarish.cs8@gmail.com>
2024-12-11 14:05:03 +0530

    Add startNewChat

--- app/(guest)/login/chatbot/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ChatbotPage() {
-  return (
-    <div>ChatbotPage</div>
-  )
-}
-
-export default ChatbotPage
---
+++ lib/startNewChat.ts
@@ -0,0 +1,8 @@
+
+async function startNewChat(
+     guestName:string,
+     guestEmail:string,
+     chatbotId:number
+){
+    
+}
